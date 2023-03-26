d3233bbf1b4755b92b25c0286070927c4ae82e66
IamGOOD <rukshenas1990@gmail.com>
2023-03-26 00:25:17 -0400

    finally & else

---
+++ Excepciones/finally_esle.py
@@ -0,0 +1,47 @@
+# try:
+#     a, b = map(int, input().split())
+#     res = a / b
+#
+# except ValueError as k:
+#     print(k)
+#
+# except ZeroDivisionError as k:
+#     print(k) # >>> division by zero
+#
+# # is no mistakes blok esle will worked
+# else:
+#     print('finished')
+#
+# # always work, even if no bugs
+# finally:
+#     print('Finally program is finished')
+
+
+#         f = open('file.txt')
+#         f.write('!!!')
+#
+# except FileNotFoundError as k:
+#     print(k)
+#
+# except:
+#     print('Another mistake')
+#
+# finally:
+#     if f:
+#         f.close()
+#         print('file closed')
+#
+
+def get_values():
+    try:
+        x, y = map(int, input().split())
+        return x, y
+    except ValueError as k:
+        print(k)
+        return 0, 0
+
+    finally:
+        print('I work before return')
+
+x, y = get_values()
+print(x, y)
